761d3c0c98cabc6af8620a1ecc3e89f0806a1aec
Geraldo Costa Filho <92253544+geraldotech@users.noreply.github.com>
2024-04-14 10:13:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
 
 server.get("/echo", (req, res) => {
-  res.json(req.query);
-  console.log(res);
+ // res.json(req.query);
+    // Create a JSON object to send as response
+  const response = {
+    message: "This is an echo response",
+    queryParameters: req.query
+  };
+  
+  // Send the JSON object as response
+  res.json(response);
 });
 
 server.use(middlewares);
